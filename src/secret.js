70505c1f1d57fb67e8c463391a81ea138f618456
cmacbryde <93786196+cmacbryde@users.noreply.github.com>
2021-12-05 16:57:56 -0500

    Update secret.js

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYYYY";  // fill in your info
+var airtable_api = "key" + "ksm8rMFjxhWkqo";     // fill in your info
+var airtable_baseid = "app" + "fknRESf3Kq5RVX";  // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
